6f43a0ab91979ea4d5f6f9a9cdc8ea2acdbd5c73
Dobroposter <smit.1t@yandex.ru>
2024-08-14 19:03:36 +0700

    Update 0.4.0 - the author's birthday update

--- git.bat
+++ git.bat
@@ -1,4 +1,5 @@
 @echo off
-# yes
-echo yes
+"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update 0.4.0 - the author's birthday update"
+"C:\Program Files\Git\cmd\git.exe" push origin main
 pause
