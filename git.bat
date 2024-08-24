885be584985d588742ca0ad7580488cdc3abe846
Dobroposter <smit.1t@yandex.ru>
2024-08-24 19:29:12 +0700

    Update 0.4.5 - the Optifix update

--- git.bat
+++ git.bat
@@ -1,5 +1,5 @@
 @echo off
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Update 0.4.0 - the author's birthday update"
+"C:\Program Files\Git\cmd\git.exe" commit -m "Update 0.4.5 - the Optifix update"
 "C:\Program Files\Git\cmd\git.exe" push origin main
 pause
